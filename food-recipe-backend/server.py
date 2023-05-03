03b0521099b2a0b56619afecb1e4a78292e81914
ProJBH <nzbohuajia@gmail.com>
2023-05-03 09:50:24 +0200

    Update server.py
    adding comments

--- food-recipe-backend/server.py
+++ food-recipe-backend/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+#This line creates a new Flask application instance and assigns it to the variable app
 app = Flask(__name__)
+
+#This line initializes the CORS extension for the Flask application, allowing Cross-Origin Resource Sharing (CORS) for all routes.
 CORS(app)
 
+#This line is a route decorator that binds the following function calculate_recipe() to the specified route (/calculate-recipe) with the POST method.
 @app.route('/calculate-recipe', methods=['POST'])
 def calculate_recipe():
+    #This line extracts the dishes property from the JSON payload of the incoming HTTP request and assigns it to the variable selected_dishes
     selected_dishes = request.json['dishes']
     total_amount = 0
     detailed_recipe = []
@@ -17,6 +22,7 @@ def calculate_recipe():
             'unitPrice': dish['price']
         })
 
+    #This line uses the jsonify helper function to convert a dictionary containing the totalAmount and detailedRecipe properties to JSON format, and returns it as the HTTP response
     return jsonify({'totalAmount': total_amount, 'detailedRecipe': detailed_recipe})
 
 if __name__ == '__main__':
